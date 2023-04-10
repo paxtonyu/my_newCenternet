9fed75026acdafa435ef8e6adb03ba68d6564e6b
paxtonyu <puchyu@163.com>
2023-04-10 19:39:50 +0800

    调整训练策略

--- mytools/my_coco_register.py
+++ mytools/my_coco_register.py
@@ -4,5 +4,5 @@ def register_my_coco():
     register_coco_instances("my_indoor_dataset_train", {}, "datasets/coco_my/annotations/my_instances_train2017.json", "datasets/coco_my/train2017")
     register_coco_instances("my_indoor_dataset_val", {}, "datasets/coco_my/annotations/my_instances_val2017.json", "datasets/coco_my/val2017")
 
-    register_coco_instances("coco_2017_train", {}, "datasets/coco_my/annotations/instances_train2017.json", "datasets/coco_my/train2017")
-    register_coco_instances("coco_2017_val", {}, "datasets/coco_my/annotations/instances_val2017.json", "datasets/coco_my/val2017")
+    # register_coco_instances("coco_2017_train", {}, "datasets/coco_my/annotations/instances_train2017.json", "datasets/coco_my/train2017")
+    # register_coco_instances("coco_2017_val", {}, "datasets/coco_my/annotations/instances_val2017.json", "datasets/coco_my/val2017")
