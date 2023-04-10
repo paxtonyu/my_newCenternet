929524cacf36f268112d34bb6b386b54614dd1c9
paxtonyu <puchyu@163.com>
2023-04-10 19:01:26 +0800

    更新backbone

--- mytools/my_coco_register.py
+++ mytools/my_coco_register.py
@@ -1,5 +1,8 @@
 from detectron2.data.datasets import register_coco_instances
 
 def register_my_coco():
-    register_coco_instances("my_indoor_dataset_train", {}, "datasets/coco_my/annotations/instances_train2017.json", "datasets/coco_my/train2017")
-    register_coco_instances("my_indoor_dataset_val", {}, "datasets/coco_my/annotations/instances_val2017.json", "datasets/coco_my/val2017")
+    register_coco_instances("my_indoor_dataset_train", {}, "datasets/coco_my/annotations/my_instances_train2017.json", "datasets/coco_my/train2017")
+    register_coco_instances("my_indoor_dataset_val", {}, "datasets/coco_my/annotations/my_instances_val2017.json", "datasets/coco_my/val2017")
+
+    register_coco_instances("coco_2017_train", {}, "datasets/coco_my/annotations/instances_train2017.json", "datasets/coco_my/train2017")
+    register_coco_instances("coco_2017_val", {}, "datasets/coco_my/annotations/instances_val2017.json", "datasets/coco_my/val2017")
