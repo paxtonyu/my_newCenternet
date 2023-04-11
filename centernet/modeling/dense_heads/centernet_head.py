4acff842dc23f9c3353796ef0b194463e61c6581
paxtonyu <puchyu@163.com>
2023-04-11 16:58:13 +0800

    添加注释

--- centernet/modeling/dense_heads/centernet_head.py
+++ centernet/modeling/dense_heads/centernet_head.py
@@ -8,97 +8,101 @@ from detectron2.layers import ShapeSpec, get_norm
 from detectron2.config import configurable
 from ..layers.deform_conv import DFConv2d
 
-__all__ = ["CenterNetHead"]
+__all__ = ["CenterNetHead"] 
 
-class Scale(nn.Module):
-    def __init__(self, init_value=1.0):
-        super(Scale, self).__init__()
-        self.scale = nn.Parameter(torch.FloatTensor([init_value]))
+class Scale(nn.Module): #Scale是一个可学习的参数，用于调整bbox的大小
+    def __init__(self, init_value=1.0): #init_value是初始值
+        super(Scale, self).__init__()   #super()函数是用于调用父类(超类)的一个方法，这里是调用nn.Module的__init__()方法
+        self.scale = nn.Parameter(torch.FloatTensor([init_value]))  #nn.Parameter()是一个tensor，但是当作模型的一部分进行训练，即可学习的参数
 
-    def forward(self, input):
+    def forward(self, input):   #forward()方法是必须要有的，这里是将输入的input乘以scale
         return input * self.scale
 
-class CenterNetHead(nn.Module):
-    @configurable
-    def __init__(self, 
+class CenterNetHead(nn.Module): #CenterNetHead是CenterNet的头部，用于预测bbox和分类
+    @configurable   #configurable是一个装饰器，用于将类的参数传入类中
+    def __init__(self,  
         # input_shape: List[ShapeSpec],
-        in_channels,
-        num_levels,
-        *,
-        num_classes=80,
-        with_agn_hm=False,
-        only_proposal=False,
-        norm='GN',
-        num_cls_convs=4,
-        num_box_convs=4,
-        num_share_convs=0,
-        use_deformable=False,
-        prior_prob=0.01):
-        super().__init__()
-        self.num_classes = num_classes
-        self.with_agn_hm = with_agn_hm
-        self.only_proposal = only_proposal
-        self.out_kernel = 3
+        in_channels,    #in_channels是输入的通道数
+        num_levels,    #num_levels是输入的特征图的数量
+        *,  #*表示后面的参数必须使用关键字传参
+        num_classes=80, #num_classes是类别数
+        with_agn_hm=False,  #with_agn_hm表示是否使用agn_hm，agn_hm是用于预测是否为人的heatmap
+        only_proposal=False,    #only_proposal表示是否只预测proposal
+        norm='GN',  #norm是归一化方法，这里使用的是GroupNorm，GroupNorm是对通道进行归一化
+        num_cls_convs=4,    #num_cls_convs是分类的卷积层数
+        num_box_convs=4,    #num_box_convs是bbox的卷积层数
+        num_share_convs=0,  #num_share_convs是共享的卷积层数
+        use_deformable=False,   #use_deformable表示是否使用可变形卷积
+        prior_prob=0.01):   #prior_prob是先验概率
+        super().__init__()  #调用父类的__init__()方法
+        self.num_classes = num_classes  #将num_classes赋值给self.num_classes
+        self.with_agn_hm = with_agn_hm  #将with_agn_hm赋值给self.with_agn_hm
+        self.only_proposal = only_proposal  #将only_proposal赋值给self.only_proposal
+        self.out_kernel = 3 #out_kernel是输出的卷积核大小
 
-        head_configs = {
+        head_configs = {    #head_configs是头部的配置
             "cls": (num_cls_convs if not self.only_proposal else 0, \
-                use_deformable),
-            "bbox": (num_box_convs, use_deformable),
-            "share": (num_share_convs, use_deformable)}
+                use_deformable),    #cls是分类
+            "bbox": (num_box_convs, use_deformable),    #bbox是bbox
+            "share": (num_share_convs, use_deformable)} #share是共享的
 
         # in_channels = [s.channels for s in input_shape]
         # assert len(set(in_channels)) == 1, \
         #     "Each level must have the same channel!"
         # in_channels = in_channels[0]
         channels = {
-            'cls': in_channels,
+            'cls': in_channels, 
             'bbox': in_channels,
             'share': in_channels,
         }
-        for head in head_configs:
-            tower = []
+        for head in head_configs:   # 遍历head_configs
+            tower = []  # tower是一个列表，列表中的元素是nn.Sequential()的参数
+            # nn.Sequential()是一个顺序容器，网络中的层将按照传递给构造函数的顺序被添加到计算图中
+            # 当传递给forward()的输入数据流经这些层时，将产生输出数据
             num_convs, use_deformable = head_configs[head]
             channel = channels[head]
-            for i in range(num_convs):
-                if use_deformable and i == num_convs - 1:
-                    conv_func = DFConv2d
+            for i in range(num_convs):  
+                if use_deformable and i == num_convs - 1:   #如果使用可变形卷积且是最后一层卷积
+                    conv_func = DFConv2d    #使用DFConv2d，DFConv2d是可变形卷积
                 else:
-                    conv_func = nn.Conv2d
-                tower.append(conv_func(
-                        in_channels if i == 0 else channel,
-                        channel, 
-                        kernel_size=3, stride=1,
-                        padding=1, bias=True
-                ))
-                if norm == 'GN' and channel % 32 != 0:
-                    tower.append(nn.GroupNorm(25, channel))
-                elif norm != '':
-                    tower.append(get_norm(norm, channel))
-                tower.append(nn.ReLU())
-            self.add_module('{}_tower'.format(head),
-                            nn.Sequential(*tower))
+                    conv_func = nn.Conv2d   #否则使用nn.Conv2d，nn.Conv2d是普通卷积
+                tower.append(conv_func( #添加卷积层，conv_func是卷积层，是nn.Conv2d()或DFConv2d()
+                        in_channels if i == 0 else channel, 
+                        channel,    
+                        kernel_size=3, stride=1,    #卷积核大小为3，步长为1
+                        padding=1, bias=True    #padding为1，bias为True
+                ))  # nn.Conv2d()是一个二维卷积层
+                # in_channels是输入的通道数，channel是输出的通道数，kernel_size是卷积核大小
+                # stride是步长，padding是填充，bias是是否使用偏置
+                if norm == 'GN' and channel % 32 != 0:  #如果使用GroupNorm且channel不能被32整除
+                    tower.append(nn.GroupNorm(25, channel)) #使用nn.GroupNorm，GroupNorm是对通道进行归一化
+                elif norm != '':    #否则如果norm不为空
+                    tower.append(get_norm(norm, channel))   #使用get_norm()方法，get_norm()方法是获取归一化方法
+                tower.append(nn.ReLU()) #添加激活函数
+            self.add_module('{}_tower'.format(head),    #添加模块，模块名为head_tower
+                            nn.Sequential(*tower))  #添加tower，tower是一个列表，列表中的元素是nn.Sequential()的参数
 
-        self.bbox_pred = nn.Conv2d(
-            in_channels, 4, kernel_size=self.out_kernel,
-            stride=1, padding=self.out_kernel // 2
+        self.bbox_pred = nn.Conv2d( #添加bbox_pred，bbox_pred是用于预测bbox的卷积层
+            in_channels, 4, kernel_size=self.out_kernel,  #输入通道数为in_channels，输出通道数4，卷积核大小为out_kernel
+            stride=1, padding=self.out_kernel // 2  #步长为1，padding为out_kernel // 2
         )
 
-        self.scales = nn.ModuleList(
-            [Scale(init_value=1.0) for _ in range(num_levels)])
+        self.scales = nn.ModuleList(    #添加scales，scales是一个列表，列表中的元素是Scale()的参数
+            [Scale(init_value=1.0) for _ in range(num_levels)]) #Scale()是用于缩放特征图的类
 
-        for modules in [
-            self.cls_tower, self.bbox_tower,
-            self.share_tower,
-            self.bbox_pred,
+        for modules in [    #遍历[cls_tower, bbox_tower, share_tower, bbox_pred]
+            self.cls_tower, self.bbox_tower,    #cls_tower是分类的卷积层，bbox_tower是bbox的卷积层
+            self.share_tower,   #share_tower是共享的卷积层
+            self.bbox_pred, #bbox_pred是用于预测bbox的卷积层
         ]:
-            for l in modules.modules():
-                if isinstance(l, nn.Conv2d):
-                    torch.nn.init.normal_(l.weight, std=0.01)
-                    torch.nn.init.constant_(l.bias, 0)
+            for l in modules.modules(): #遍历modules.modules()
+                if isinstance(l, nn.Conv2d):    #如果l是nn.Conv2d()，nn.Conv2d()是一个二维卷积层
+                    torch.nn.init.normal_(l.weight, std=0.01)   #使用torch.nn.init.normal_()初始化权重，std是标准差
+                    torch.nn.init.constant_(l.bias, 0)  #使用torch.nn.init.constant_()初始化偏置，bias是偏置
         
-        torch.nn.init.constant_(self.bbox_pred.bias, 8.)
-        prior_prob = prior_prob
-        bias_value = -math.log((1 - prior_prob) / prior_prob)
+        torch.nn.init.constant_(self.bbox_pred.bias, 8.)    # bbox_pred.bias是偏置，初始化为8
+        prior_prob = prior_prob # prior_prob是先验概率
+        bias_value = -math.log((1 - prior_prob) / prior_prob)   # bias_value计算公式为-log((1 - prior_prob) / prior_prob)
 
         if self.with_agn_hm:
             self.agn_hm = nn.Conv2d(
--- centernet/modeling/meta_arch/centernet_detector.py
+++ centernet/modeling/meta_arch/centernet_detector.py
@@ -10,60 +10,60 @@ from detectron2.modeling import detector_postprocess
 from detectron2.structures import ImageList
 
 @META_ARCH_REGISTRY.register()
-class CenterNetDetector(nn.Module):
-    def __init__(self, cfg):
-        super().__init__()
-        self.mean, self.std = cfg.MODEL.PIXEL_MEAN, cfg.MODEL.PIXEL_STD
-        self.register_buffer("pixel_mean", torch.Tensor(cfg.MODEL.PIXEL_MEAN).view(-1, 1, 1))
-        self.register_buffer("pixel_std", torch.Tensor(cfg.MODEL.PIXEL_STD).view(-1, 1, 1))
+class CenterNetDetector(nn.Module): #这是一个类，继承自nn.Module，nn.Module是所有神经网络模块的基类，里面可以放各种各样的网络层
+    def __init__(self, cfg):    #初始化函数，cfg是配置文件
+        super().__init__() #调用父类的构造函数
+        self.mean, self.std = cfg.MODEL.PIXEL_MEAN, cfg.MODEL.PIXEL_STD #获取像素均值和标准差
+        self.register_buffer("pixel_mean", torch.Tensor(cfg.MODEL.PIXEL_MEAN).view(-1, 1, 1)) #将像素均值转换为张量
+        self.register_buffer("pixel_std", torch.Tensor(cfg.MODEL.PIXEL_STD).view(-1, 1, 1)) #将像素标准差转换为张量
         
-        self.backbone = build_backbone(cfg)
-        self.proposal_generator = build_proposal_generator(
+        self.backbone = build_backbone(cfg) #构建backbone
+        self.proposal_generator = build_proposal_generator( #根据backbone的output_shape构建proposal_generator
             cfg, self.backbone.output_shape()) # TODO: change to a more precise name
     
-    
-    def forward(self, batched_inputs):
-        if not self.training:
-            return self.inference(batched_inputs)
-        images = self.preprocess_image(batched_inputs)
-        features = self.backbone(images.tensor)
-        gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
+    #前向传播函数，输入为batched_inputs，是一个列表，每个元素是一个字典，字典中包含了图像的信息，如图像的路径、图像的尺寸等，这些信息都是在dataset中定义的
+    def forward(self, batched_inputs):  #训练的时候要调用这个函数来计算损失函数，测试的时候，需要调用这个函数来计算预测结果
+        if not self.training:   #如果不是训练阶段，就调用inference函数来计算预测结果
+            return self.inference(batched_inputs)   #返回预测结果
+        images = self.preprocess_image(batched_inputs)  #如果是训练阶段，就调用preprocess_image函数来对图像进行预处理
+        features = self.backbone(images.tensor) #将预处理后的图像输入backbone，得到backbone的输出
+        gt_instances = [x["instances"].to(self.device) for x in batched_inputs] #将ground truth的信息转换为张量
 
-        _, proposal_losses = self.proposal_generator(
-            images, features, gt_instances)
+        _, proposal_losses = self.proposal_generator(   #调用proposal_generator来计算损失函数
+            images, features, gt_instances) #输入图像、backbone的输出、ground truth的信息，得到损失函数
         return proposal_losses
 
 
-    @property
+    @property   
     def device(self):
         return self.pixel_mean.device
 
 
-    @torch.no_grad()
-    def inference(self, batched_inputs, do_postprocess=True):
-        images = self.preprocess_image(batched_inputs)
-        inp = images.tensor
-        features = self.backbone(inp)
-        proposals, _ = self.proposal_generator(images, features, None)
+    @torch.no_grad()    #不计算梯度
+    def inference(self, batched_inputs, do_postprocess=True):   #计算预测结果
+        images = self.preprocess_image(batched_inputs)  #预处理图像
+        inp = images.tensor #获取预处理后的图像
+        features = self.backbone(inp)   #将预处理后的图像输入backbone，得到backbone的输出
+        proposals, _ = self.proposal_generator(images, features, None)  #调用proposal_generator来计算预测结果
 
-        processed_results = []
-        for results_per_image, input_per_image, image_size in zip(
+        processed_results = []  #存储预测结果
+        for results_per_image, input_per_image, image_size in zip( #遍历图像库，zip函数将可迭代的对象作为参数，将其对应元素打包成一个个元组，返回这些元组组成的列表
             proposals, batched_inputs, images.image_sizes):
-            if do_postprocess:
-                height = input_per_image.get("height", image_size[0])
-                width = input_per_image.get("width", image_size[1])
-                r = detector_postprocess(results_per_image, height, width)
-                processed_results.append({"instances": r})
+            if do_postprocess:  #如果需要后处理
+                height = input_per_image.get("height", image_size[0])   #获取图像的高度
+                width = input_per_image.get("width", image_size[1]) #获取图像的宽度
+                r = detector_postprocess(results_per_image, height, width)  #调用后处理函数
+                processed_results.append({"instances": r})  #将后处理后的结果存储到processed_results中
             else:
-                r = results_per_image
-                processed_results.append(r)
-        return processed_results
+                r = results_per_image   #如果不需要后处理，就直接将预测结果存储到processed_results中
+                processed_results.append(r) #将预测结果存储到processed_results中
+        return processed_results    #返回预测结果
 
-    def preprocess_image(self, batched_inputs):
+    def preprocess_image(self, batched_inputs):  #预处理图像
         """
         Normalize, pad and batch the input images.
         """
-        images = [x["image"].to(self.device) for x in batched_inputs]
-        images = [(x - self.pixel_mean) / self.pixel_std for x in images]
-        images = ImageList.from_tensors(images, self.backbone.size_divisibility)
-        return images
+        images = [x["image"].to(self.device) for x in batched_inputs]   #将图像转换为张量
+        images = [(x - self.pixel_mean) / self.pixel_std for x in images]   #对图像进行归一化
+        images = ImageList.from_tensors(images, self.backbone.size_divisibility)    #将图像转换为ImageList
+        return images   #返回预处理后的图像
