3192657c282fddabd13aab9c8a596954e79f1c99
paxtonyu <puchyu@163.com>
2023-04-20 21:34:25 +0800

    注册室内数据集

--- mytools/my_coco_register.py
+++ mytools/my_coco_register.py
@@ -1,4 +1,4 @@
-from detectron2.data.datasets import load_coco_json
+from detectron2.data.datasets import register_coco_instances
 from detectron2.data import DatasetCatalog, MetadataCatalog
 import os
 
@@ -10,18 +10,5 @@ coco_50 = ['person','backpack','umbrella','handbag','tie','suitcase',
  'refrigerator', 'book','clock','vase','scissors','teddy bear','hair drier','toothbrush',]
 
 def register_my_coco():
-    register_coco_instances_v2("my_indoor_dataset_train", {}, "datasets/coco_my/annotations/my_instances_train2017.json", "datasets/coco_my/train2017")
-    register_coco_instances_v2("my_indoor_dataset_val", {}, "datasets/coco_my/annotations/my_instances_val2017.json", "datasets/coco_my/val2017")
-
-def register_coco_instances_v2(name, metadata, json_file, image_root):
-    assert isinstance(name, str), name
-    assert isinstance(json_file, (str, os.PathLike)), json_file
-    assert isinstance(image_root, (str, os.PathLike)), image_root
-    # 1. register a function which returns dicts
-    DatasetCatalog.register(name, lambda: load_coco_json(json_file, image_root, name))
-    # 2. Optionally, add metadata about this dataset,
-    # since they might be useful in evaluation, visualization or logging
-    MetadataCatalog.get(name).set(
-        json_file=json_file, image_root=image_root, evaluator_type="coco", **metadata
-    )
-    MetadataCatalog.get(name).thing_classes = coco_50
+    register_coco_instances("my_indoor_dataset_train", {}, "datasets/coco_my/annotations/my_instances_train2017.json", "datasets/coco_my/train2017")
+    register_coco_instances("my_indoor_dataset_val", {}, "datasets/coco_my/annotations/my_instances_val2017.json", "datasets/coco_my/val2017")
